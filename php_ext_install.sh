714d6a7af3ed9a1c28c19d6f61d6603d0132e4aa
rootsecurity <rootsecurity@163.com>
2013-08-29 17:04:12 +0800

    Update php_ext_install.sh

--- php_ext_install.sh
+++ php_ext_install.sh
@@ -2,13 +2,15 @@
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export LANG="en_US.UTF-8"
 
+
+
 ##########################################
-##					                            ##
-##	$Name: php_ext_install.sh	          ##
-##	$Time: 2013/06/19		                ##
-##	$Version:0.1.8           	          ##
+##					                    ##
+##	$Name: php_ext_install.sh	        ##
+##	$Time: 2013/06/19		            ##
+##	$Version:0.1.8           	        ##
 ##  $Author:rootsecurity                ##
-##					                            ##
+##					                    ##
 ##########################################
 
 ##########################################
