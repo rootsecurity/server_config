ce4e0e4bd90eac311a164ca62ec9ffd14b209c9e
rootsecurity <rootsecurity@163.com>
2014-04-08 11:30:37 +0800

    Create install_trafficserver_v2.0.sh

---
+++ install_trafficserver_v2.0.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# 
+# install_trafficserver_v2.0.sh
+# Author:@rootsecurity
+# Date:2013-09
+# Usage:install_trafficserver_v2.0.sh (init|install|uninstall)
+
+TVER="4.1.2"
+
+function_init() {
+	for ats_packages in tcl tcl-devel pcre pcre-devel expat-devel libcap-devel gcc-c++ lzma make file file-devel;
+	do
+	yum -y install $ats_packages;
+	done
+}
+
+function_install() {
+	wget -c http://mirror.bit.edu.cn/apache/trafficserver/trafficserver-${TVER}.tar.bz2
+	
+	tar jxvf trafficserver-${TVER}.tar.gz
+	cd trafficserver-${TVER}/
+	./configure --prefix=/usr/local/trafficserver-${TVER} --enable-static \
+	--enable-tproxy=force \
+	--enable-posix-cap \
+	--enable-shared \
+	--enable-wccp \
+	--with-zlib \
+	--with-pcre
+	make && make install
+	cd ../
+}
+
+function_uninstall() {
+	/usr/local/trafficserver-${TVER}/bin/trafficserver stop
+	rm -rf /usr/local/trafficserver-${TVER}
+}
+
+case $1 in 
+   init)
+	function_init
+	;;
+   install)
+	function_install
+	;;
+   uninstall)
+	function_uninstall
+	;;
+   *)
+	echo "Usage:$0 {init|install|uninstall}"
+	exit 1
+esac
