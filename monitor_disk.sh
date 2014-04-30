594ae4b4422eeb182c6ca99a2100cc4a45a482eb
rootsecurity <rootsecurity@163.com>
2014-04-30 18:42:00 +0800

    Update monitor_disk.sh

--- monitor_disk.sh
+++ monitor_disk.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-i=`df -h /var/log/http.log | grep G | awk '{print $3}' | awk -F"[.G]" '{print $1}'`
-if [ $i -gt 10 ] ; then
-        cat /dev/null > /var/log/http.log
+#监控/var/log/suricata/http.log
+i=`du /var/log/suricata/http.log |awk '{print $1}'`
+if [ $i -ge 512000 ] ; then
+        cat /dev/null > /var/log/suricata/http.log
+fi
+
+#监控/var/log/suricata/fast.log
+g=`du /var/log/suricata/fast.log | awk '{print $1}'`
+if [ $g -ge 204800 ] ; then
+        cat /dev/null > /var/log/suricata/fast.log
+fi
+
+#监控/var/log/suricata/stats.log
+q=`du /var/log/suricata/stats.log | awk '{print $1}'`
+if [ $q -ge 204800 ] ; then
+        cat /dev/null > /var/log/suricata/stats.log
 fi
