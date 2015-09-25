405d3eba30d5851091e00a0a771c4e1444422839
rootsecurity <rootsecurity@163.com>
2015-09-25 14:37:04 +0800

    Update monitor_disk.sh

--- monitor_disk.sh
+++ monitor_disk.sh
@@ -5,10 +5,10 @@
 SHIYONGLV=`df | grep var | grep -v wd/ | grep -c -E \([7-9][0-9]\%\)\|\(100\%\)`
 
 if [ $SHIYONGLV -gt 0 ]; then
-        /bin/rm -rf /var/wd/hadoop/tmp/dfs/data/current/subdir*
-        /bin/rm -rf /var/wd/hadoop/tmp/dfs/data/current/blk*
-        echo -e "\033[1;32;40m硬盘清理完毕\033[0m"
+	/bin/rm -rf /var/wd/hadoop/tmp/dfs/data/current/subdir*
+	/bin/rm -rf /var/wd/hadoop/tmp/dfs/data/current/blk*
+	echo -e "\033[1;32;40m `date '+%Y-%m-%d %H:%M:%S\033[0m'`\033[0m" "\033[1;32;40m硬盘空间清理完毕\033[0m"
 else
-        echo -e "\033[1;32;40m硬盘空间合理，无需操作\033[0m"
+	echo -e "\033[1;32;40m `date '+%Y-%m-%d %H:%M:%S\033[0m'`\033[0m" "\033[1;32;40m硬盘空间合理，无需操作\033[0m"
 fi
 sleep 3
