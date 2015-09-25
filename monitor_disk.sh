e96ef732bb1c3f4eab76253ee8e9b8ed8ae051e1
rootsecurity <rootsecurity@163.com>
2015-09-25 13:37:14 +0800

    Create monitor_disk.sh

---
+++ monitor_disk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 监控硬盘/var/wd分区使用
+
+#当/var/wd/使用大于70%时SHIYONGLV为1，否则为0
+SHIYONGLV=`df | grep var | grep -v wd/ | grep -c -E \([7-9][0-9]\%\)\|\(100\%\)`
+
+if [ $SHIYONGLV -gt 0 ]; then
+        /bin/rm -rf /var/wd/hadoop/tmp/dfs/data/current/subdir*
+        /bin/rm -rf /var/wd/hadoop/tmp/dfs/data/current/blk*
+        echo -e "\033[1;32;40m硬盘清理完毕\033[0m"
+else
+        echo -e "\033[1;32;40m硬盘空间合理，无需操作\033[0m"
+fi
+sleep 3
